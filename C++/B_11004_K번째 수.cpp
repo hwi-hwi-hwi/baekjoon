f16a4c424ce4ed56de4d105b45966863b5c22be2
hwi-hwi-hwi <venusksh224@gmail.com>
2024-12-31 23:49:05 +0900

    12.31(화)
    백준

---
+++ C++/B_11004_K번째 수.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+	
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n, k;
+	cin >> n >> k;
+	
+	vector<int> v(n);
+	for(int i = 0; i < n; i++){
+		cin >> v[i];
+	}
+	sort(v.begin(), v.end());
+	
+	cout << v[k-1];
+	
+	return 0;
+}
