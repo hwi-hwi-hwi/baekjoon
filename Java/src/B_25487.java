4d36085f028f3864530e7ddfb1cbc0a2bc7df685
hwi-hwi-hwi <venusksh224@gmail.com>
2025-08-02 00:08:40 +0900

    2025-08-01 백준 Java

--- Java/src/B_25487.java
+++ Java/src/B_25487.java
@@ -1,19 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class B_25487 {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int t = sc.nextInt();
+        int t = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder(t * 3);
+
         for (int i = 0; i < t; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
             int ans = Math.min(a, Math.min(b, c));
 
-            System.out.println(ans);
+            sb.append(ans).append('\n');
         }
+
+        System.out.println(sb);
     }
 }
